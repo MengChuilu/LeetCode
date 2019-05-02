618f9dd382118edc9e8a62c41bcc3dfbe360f9f9
孟垂露 <mengchuilu@baidu.com>
2019-05-02 19:25:17 +0800

    q012

---
+++ src/main/java/q012/Solution.java
@@ -0,0 +1,25 @@
+package q012;
+
+class Solution {
+    public String intToRoman(int num) {
+        int[] values= new int[]{1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String reps[]=new String[]{"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        String result = "";
+        while (num > 0){
+            for (int i = 0 ;i < 13;i ++) {
+                if (num >= values[i]) {
+                    result += reps[i];
+                    num -= values[i];
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.intToRoman(3));
+    }
+}
