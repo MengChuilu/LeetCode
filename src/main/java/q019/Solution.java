687d7d2cda4218972b015c465b85fa7fa3ad0478
孟垂露 <mengchuilu@baidu.com>
2019-05-03 20:16:22 +0800

    q019

---
+++ src/main/java/q019/Solution.java
@@ -0,0 +1,29 @@
+package q019;
+
+import base.ListNode;
+
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode first = dummy;
+        ListNode second = dummy;
+
+        for (int i = 0; i < n + 1; i++) {
+            first = first.next;
+        }
+
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.removeNthFromEnd(new ListNode(1), 1);
+    }
+}
