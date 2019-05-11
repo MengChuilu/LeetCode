8099ebc93aa8d585bd5916b2a5cf6ed7f9655dba
孟垂露 <mengchuilu@baidu.com>
2019-05-11 12:23:04 +0800

    q029

---
+++ src/main/java/q029/Solution.java
@@ -0,0 +1,37 @@
+package q029;
+
+class Solution {
+    public int divide(int dividend, int divisor) {
+
+        int sign = (dividend ^ divisor) >> 31;
+        long lDividend = Math.abs((long) dividend);
+        long lDivisor = Math.abs((long) divisor);
+        long res = 0;
+
+        while (lDividend >= lDivisor){
+            long tmp = lDivisor;
+            long i = 1;
+            while (lDividend >= tmp){
+                lDividend -= tmp;
+                res += i;
+                i <<= 1;
+                tmp <<= 1;
+            }
+        }
+        if (sign == -1) {
+            res *= -1;
+        }
+        if (res < Integer.MIN_VALUE) {
+            return Integer.MIN_VALUE;
+        } else if (res > Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+        return (int)res;
+
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.divide(1, 1);
+    }
+}
