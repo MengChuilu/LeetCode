72deab418f8a9fd0d1eeadf0d57207b9b90b158e
孟垂露 <570045412@qq.com>
2020-02-25 11:18:30 +0800

    update .gitignore

---
+++ src/main/java/r233/Solution.java
@@ -0,0 +1,16 @@
+package r233;
+
+import com.sun.tools.javac.util.List;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Solution {
+    public List<String> removeSubfolders(String[] folder) {
+
+        List<String> result = new ArrayList<>();
+        Arrays.sort(folder);
+
+        for (int )
+    }
+}
